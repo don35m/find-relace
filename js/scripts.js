ee1ea27fa69fc9625f716b893feaa84167973843
Don Schemmel <dons500@gmail.com>
2015-09-11 12:44:13 -0700

    Add forms and Jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
 var findReplace = function(string, word, replace) {
+  //use reg exp to find, replace
   var result = string.replace(new RegExp(word, 'ig'), replace);
+  //incase of no match
   if (result == string) {
     return "No Match";
   } else {
     return result;
   }
 };
+
+$(document).ready(function() {
+  $("form#findReplace").submit(function(event) {
+    var string = $("input#string").val();
+    var word = $("input#word").val();
+    var replace = $("input#replace").val();
+    var result = findReplace(string, word, replace);
+
+    $("#results span").text(result);
+    $("#results").show();
+
+
+    event.preventDefault();
+  });
+});
